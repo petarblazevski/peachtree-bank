43f24887702b0bad91a4cc077e7277d4f96e1bee
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-06-04 23:04:55 +0200

    add filters and transactions list

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,5 +1,13 @@
 import { HeaderComponent } from './header/header.component';
+import { FiltersComponent } from './filters/filters.component';
+import { TransactionListComponent } from './transaction-list/transaction-list.component';
 
-export const components: any[] = [HeaderComponent];
+export const components: any[] = [
+  HeaderComponent,
+  FiltersComponent,
+  TransactionListComponent,
+];
 
 export * from './header/header.component';
+export * from './filters/filters.component';
+export * from './transaction-list/transaction-list.component';
--- src/app/containers/index.ts
+++ src/app/containers/index.ts
@@ -1,5 +1,7 @@
 import { AppComponent } from './app/app.component';
+import { TransactionsComponent } from './transactions/transactions.component';
 
-export const containers: any[] = [AppComponent];
+export const containers: any[] = [AppComponent, TransactionsComponent];
 
 export * from './app/app.component';
+export * from './transactions/transactions.component';
