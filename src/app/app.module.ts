1fbbd4cfad3585ce03b94e659643467c48cd0191
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-06-04 17:34:59 +0200

    add @ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import * as fromContainers from './containers';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [...fromContainers.containers],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
+  ],
   providers: [],
   bootstrap: [fromContainers.AppComponent],
 })
