b664d0a4a59a3c2ac98a9b0222e57b86dd8a0666
Petar Blazhevski <pblazhevski@mobiquityinc.com>
2020-06-07 23:54:38 +0200

    reorder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { StoreModule } from '@ngrx/store';
@@ -13,7 +14,6 @@ import { environment } from '../environments/environment';
 
 import { TransactionEffects } from './store/transaction.effects';
 import { reducers } from './store';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [...fromContainers.containers, ...fromComponents.components],
